2db9ef5eb5dfc48f791e9913ead1e3c7db848235
Sean Carlisle <scarlisle@hylamobile.com>
2019-01-15 14:31:23 -0600

    some fixin

--- src/main/java/org/mavriksc/messin/ArraysAsList.java
+++ src/main/java/org/mavriksc/messin/ArraysAsList.java
@@ -1,13 +1,21 @@
 package org.mavriksc.messin;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ArraysAsList {
 
 
     public static void main (String... args){
-        List<Long> l = Arrays.asList(new Long(100),new Long(500));
-        l.forEach(lon->System.out.println(lon));
+        List<Long> l = Arrays.asList(100L, 500L);
+        l.forEach(System.out::println);
+        List<Long> in = l.stream().filter(lo-> 0<lo&& lo<10).collect(Collectors.toList());
+        List<Long> out = new ArrayList<>(l);
+        out.removeAll(in);
+
+
     }
 }
