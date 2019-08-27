06913259793e403d22fe2971b29b8ba840563070
Sean Carlisle <scarlisle@hylamobile.com>
2019-08-27 17:03:12 -0500

    asdf

---
+++ src/main/java/org/mavriksc/messin/hackerrank/VarArgs.java
@@ -0,0 +1,24 @@
+package org.mavriksc.messin.hackerrank;
+
+import java.util.Arrays;
+
+public class VarArgs {
+    public static void main(String[] args) {
+
+    }
+}
+class Add{
+    Add(){
+
+    }
+    public void add(int... vals){
+        int sum=0;
+        StringBuilder sb = new StringBuilder();
+        for (int val : vals) {
+            sum += val;
+            sb.append(val).append("+");
+        }
+        sb.deleteCharAt(sb.lastIndexOf("+")).append("=").append(sum);
+        System.out.println(sum);
+    }
+}
