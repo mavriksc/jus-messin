8bbb6178b4fcca756c316863768294481044bddb
Sean Carlisle <scarlisle@hylamobile.com>
2019-08-27 14:23:32 -0500

    asdf

--- src/main/java/org/mavriksc/messin/CheckListType.java
+++ src/main/java/org/mavriksc/messin/CheckListType.java
@@ -7,9 +7,9 @@ import java.util.List;
 public class CheckListType {
 
     public static void main(String[] args){
-        List<Object> list = new ArrayList<Object>();
-        list.add(Integer.valueOf(256));
-        list.add(Float.valueOf(13.5f));
+        List<Object> list = new ArrayList<>();
+        list.add(256);
+        list.add(13.5);
         list.add("this s right here");
 
         list.forEach(o-> System.out.println(o.toString()+"\t"+o.getClass()));
