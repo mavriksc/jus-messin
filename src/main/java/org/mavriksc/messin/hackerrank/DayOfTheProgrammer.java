a11d2b5e8e1aa957f0a3f625274b571a4c82ec22
Sean Carlisle <scarlisle@hylamobile.com>
2019-09-09 16:47:55 -0500

    asdf

---
+++ src/main/java/org/mavriksc/messin/hackerrank/DayOfTheProgrammer.java
@@ -0,0 +1,21 @@
+package org.mavriksc.messin.hackerrank;
+
+import java.util.Calendar;
+
+public class DayOfTheProgrammer {
+    public static void main(String[] args) {
+
+    }
+    static String getDate(int year){
+        int day;
+        int month;
+        if (year==1918){
+
+        }else if (year<1918){
+
+        }else {
+
+        }
+        return String.format("%02d.%02d.%d", day, month, year);
+    }
+}
