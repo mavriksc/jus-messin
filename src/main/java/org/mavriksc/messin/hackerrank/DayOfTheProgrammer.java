5f6a5d574f1ed4d194852946150a8c214cdc86ea
Sean Carlisle <scarlisle@hylamobile.com>
2019-09-09 16:57:45 -0500

    asdf

--- src/main/java/org/mavriksc/messin/hackerrank/DayOfTheProgrammer.java
+++ src/main/java/org/mavriksc/messin/hackerrank/DayOfTheProgrammer.java
@@ -4,6 +4,7 @@ import java.util.Calendar;
 // https://www.hackerrank.com/challenges/day-of-the-programmer/problem
 public class DayOfTheProgrammer {
     public static void main(String[] args) {
+        System.out.println(getDate(1984));
 
     }
     static String getDate(int year){
@@ -13,9 +14,12 @@ public class DayOfTheProgrammer {
             month = 10;
             day = 2;
         }else if (year<1918){
+            month = 9;
+            day = year%4==0?12:11;
 
         }else {
-
+            month = 9;
+            day = year%400==0||(year%4==0&&year%100!=0)?12:11;
         }
         return String.format("%02d.%02d.%d", day, month, year);
     }
