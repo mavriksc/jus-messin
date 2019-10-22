ce38ab4803c9508a4fb15c293cf1af0cfd4840b0
scarlisle <s.m.carlisle@gmail.com>
2019-10-22 08:55:20 -0500

    asdf

---
+++ src/main/java/org/mavriksc/messin/IterableText.java
@@ -0,0 +1,22 @@
+package org.mavriksc.messin;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+public class IterableText {
+    public static void main(String[] args) {
+        Iterable<String> list
+                = Arrays.asList("1 5 10", "1 6 20", "1 7 20", "1 8 11");
+        String answer = StreamSupport.stream(list.spliterator(),false).max(Comparator.comparingInt(IterableText::getComTime)).orElse("");
+        System.out.println(answer);
+    }
+
+    private static int getComTime(String line){
+        String[] vals = line.split(",");
+        return  Integer.parseInt(vals[2]);
+    }
+}
