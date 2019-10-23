3c35c6156a23ad7f02468e73ab5f0a72737b0c5a
Sean Carlisle <scarlisle@hylamobile.com>
2019-10-23 10:38:33 -0500

    asdf

--- src/main/java/org/mavriksc/messin/IterableText.java
+++ src/main/java/org/mavriksc/messin/IterableText.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
@@ -11,12 +12,14 @@ public class IterableText {
     public static void main(String[] args) {
         Iterable<String> list
                 = Arrays.asList("1 5 10", "1 6 20", "1 7 20", "1 8 11");
-        String answer = StreamSupport.stream(list.spliterator(),false).max(Comparator.comparingInt(IterableText::getComTime)).orElse("");
-        System.out.println(answer);
+        Optional<String> answer = StreamSupport.stream(list.spliterator(),false)
+                .max(Comparator.comparingInt(IterableText::getComTime));
+        answer.ifPresent(System.out::println);
+
     }
 
     private static int getComTime(String line){
-        String[] vals = line.split(",");
+        String[] vals = line.split(" ");
         return  Integer.parseInt(vals[2]);
     }
 }
