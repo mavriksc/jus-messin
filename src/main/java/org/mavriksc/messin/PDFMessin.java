c527ffa0dffbfb46f7400d12a174e349d35b9bf5
Sean Carlisle <scarlisle@hylamobile.com>
2019-10-24 15:44:56 -0500

    asdf

--- src/main/java/org/mavriksc/messin/PDFMessin.java
+++ src/main/java/org/mavriksc/messin/PDFMessin.java
@@ -1,5 +1,14 @@
 package org.mavriksc.messin;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+
+import java.io.IOException;
+
 public class PDFMessin {
+    public static void main(String[] args) throws IOException {
+        PDDocument doc = PDDocument.load(PDFMessin.class.getClassLoader().getResourceAsStream("pdfTest1.pdf"));
+        System.out.println("things");
+
+    }
     
 }
--- src/main/java/org/mavriksc/messin/hackerrank/HackerRank.kt
+++ src/main/java/org/mavriksc/messin/hackerrank/HackerRank.kt
@@ -3,24 +3,30 @@ package org.mavriksc.messin.hackerrank
 import kotlin.math.*
 
 fun main() {
-    val n=10
-    println(pageCount(n,10))
-    println(pageCount(n,9))
-    println(pageCount(n,8))
-    println(pageCount(n,7))
-    println(pageCount(n,6))
-    println(pageCount(n,5))
-    println(pageCount(n,4))
-    println(pageCount(n,3))
-    println(pageCount(n,2))
-    println(pageCount(n,1))
 }
 
-fun designerPdfViewer(h:Array<Int>,word:String)= word.length * word.toLowerCase().chars().map { h[it-'a'.toInt()] }.max().asInt
+fun designerPdfViewer(h: Array<Int>, word: String) = word.length * word.toLowerCase().chars().map { h[it - 'a'.toInt()] }.max().asInt
 
 fun bonAppetit(bill: Array<Int>, k: Int, b: Int) {
-    val fair = (bill.sum()-bill[k])/2 - b
-    if (fair==0) println("Bon Appetit") else println(fair)
+    val fair = (bill.sum() - bill[k]) / 2 - b
+    if (fair == 0) println("Bon Appetit") else println(fair)
 }
 
-fun pageCount(n: Int, p: Int) =min(p/2, if(n%2==0)((n-p)+1)/2 else (n-p)/2)
+fun pageCount(n: Int, p: Int) = min(p / 2, if (n % 2 == 0) ((n - p) + 1) / 2 else (n - p) / 2)
+
+
+fun countingValleys(n: Int, s: String): Int {
+    var height = 0
+    var valCount = 0
+    var oldHeight = 0
+    for (c in s) {
+        when (c) {
+            'U' -> height++
+            'D' -> height--
+        }
+        if (height == 0 && oldHeight < 0)
+            valCount++
+        oldHeight = height
+    }
+    return valCount
+}
