ca0018c4b779979bf6cd764273aaa2d5c9d848a2
scarlisle <s.m.carlisle@gmail.com>
2020-02-19 18:14:22 -0600

    asdf

--- src/main/java/org/mavriksc/messin/ResourceToString.kt
+++ src/main/java/org/mavriksc/messin/ResourceToString.kt
@@ -6,11 +6,11 @@ import java.util.*
 object ResourceToString {
     @JvmStatic
     fun main(args: Array<String>) {
-        val pi = convertStreamToString(ResourceToString::class.java.classLoader.getResourceAsStream("pi.txt"))
+        val pi = convertStreamToString(ResourceToString::class.java.classLoader.getResourceAsStream("pi.txt")!!)
         println(pi)
     }
 
-    private fun convertStreamToString(`is`: InputStream): String {
+    fun convertStreamToString(`is`: InputStream): String {
         val s = Scanner(`is`).useDelimiter("\\A")
         return if (s.hasNext()) s.next() else ""
     }
--- src/main/java/org/mavriksc/messin/hackerrank/QueensAttack2.kt
+++ src/main/java/org/mavriksc/messin/hackerrank/QueensAttack2.kt
@@ -1,13 +1,34 @@
 package org.mavriksc.messin.hackerrank
 
+import java.io.File
 import kotlin.math.min
 
 //https://www.hackerrank.com/challenges/queens-attack-2/problem
 
-// 10/22 failing
 
 fun main() {
-    print(queensAttack(8, 0, 5, 4, arrayOf(arrayOf(6, 5))))
+    val qaInput = qaInput()
+    print(queensAttack(qaInput.n, qaInput.k, qaInput.r_q, qaInput.c_q, qaInput.obstacles))
+
+}
+
+fun qaInput(): QAInput {
+    val lines = File("D:\\code\\jus-messin\\src\\main\\resources\\QA2.txt").readLines()
+    val nk = lines[0].split(' ')
+    val n = nk[0].toInt()
+    val k = nk[1].toInt()
+    val qpos = lines[1].split(' ')
+    val r_q = qpos[0].toInt()
+    val c_q = qpos[1].toInt()
+    val obstacles = mutableListOf<Array<Int>>()
+    for (i in 2 until lines.size) {
+        val rq = lines[i].split(' ')
+        obstacles.add(arrayOf(rq[0].toInt(), rq[1].toInt()))
+    }
+    return QAInput(n, k, r_q, c_q, obstacles.toTypedArray())
+}
+
+class QAInput(val n: Int, val k: Int, val r_q: Int, val c_q: Int, val obstacles: Array<Array<Int>>) {
 
 }
 
@@ -29,27 +50,27 @@ fun queensAttack(n: Int, k: Int, r_q: Int, c_q: Int, obstacles: Array<Array<Int>
         val c = it[1]
         if (c == c_q) {
             if (r < r_q) {
-                top = min(top, (r_q - r)-1)
+                top = min(top, (r_q - r) - 1)
             } else {
-                bottom = min(bottom, (r - r_q)-1)
+                bottom = min(bottom, (r - r_q) - 1)
             }
         } else if (r == r_q) {
             if (c < c_q) {
-                left = min(left, (c_q - c)-1)
+                left = min(left, (c_q - c) - 1)
             } else {
-                right = min(right, (c - c_q)-1)
+                right = min(right, (c - c_q) - 1)
             }
         } else if (r - c == queenMainDiagDistance) {
             if (r < r_q) {//tl
-                tl = min(tl, (r_q - r)-1)
+                tl = min(tl, (r_q - r) - 1)
             } else {//br
-                br = min(br, (r - r_q-1))
+                br = min(br, (r - r_q)- 1)
             }
         } else if (r + c == queenAltDiagDistance) {
-            if (r < r_q) {
-                bl = min(bl, (r - r_q)-1)
+            if (r > r_q) {
+                bl = min(bl, (r - r_q) - 1)
             } else {
-                tr = min(tr, (r_q - r)-1)
+                tr = min(tr, (r_q - r) - 1)
             }
         }
     }
