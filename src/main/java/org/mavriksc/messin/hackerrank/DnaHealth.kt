bfee8cce9878b8f1c42629d802a7691d93d5359d
scarlisle <s.m.carlisle@gmail.com>
2020-02-21 08:39:17 -0600

    asdf

--- src/main/java/org/mavriksc/messin/hackerrank/DnaHealth.kt
+++ src/main/java/org/mavriksc/messin/hackerrank/DnaHealth.kt
@@ -6,8 +6,21 @@ import java.util.*
 import kotlin.math.max
 import kotlin.math.min
 
+// working but failing all but 2 tests. need to not recalc lps
+// not recalc lps but still failing same tests timeout
+// https://www.geeksforgeeks.org/kmp-algorithm-for-pattern-searching/
+
+// IDEAS:
+//https://www.geeksforgeeks.org/suffix-tree-application-2-searching-all-patterns/
+ 
+
+// genes
+
+//make map of genes to index list
+
+
 fun main(args: Array<String>) {
-    val scan = Scanner(File("C:\\git\\mystuff\\jus-messin\\src\\main\\resources\\DNA-Input.txt"))
+    val scan = Scanner(File("D:\\code\\jus-messin\\src\\main\\resources\\DNA-2.txt"))
 
     val n = scan.nextLine().trim().toInt()
 
@@ -16,9 +29,9 @@ fun main(args: Array<String>) {
     val health = scan.nextLine().split(" ").map { it.trim().toInt() }.toTypedArray()
 
     val s = scan.nextLine().trim().toInt()
-    var low = Int.MAX_VALUE
-    var high = 0
-
+    var low = Long.MAX_VALUE
+    var high = 0L
+    val start = Date()
     for (sItr in 1..s) {
         val firstLastd = scan.nextLine().split(" ")
 
@@ -30,21 +43,27 @@ fun main(args: Array<String>) {
         val score = scoreStrand(d, first, last, genes, health)
         low = min(low, score)
         high = max(high, score)
+        counts.clear()
     }
+    val end = Date()
+    println(end.time-start.time)
     print("$low $high")
 }
-// working but failing all but 2 tests. need to not recalc lps
-//https://www.geeksforgeeks.org/kmp-algorithm-for-pattern-searching/
-fun scoreStrand(d: String, first: Int, last: Int, genes: Array<String>, health: Array<Int>): Int {
-    var score = 0
+
+var lpss: MutableMap<String, Array<Int>> = mutableMapOf()
+var counts: MutableMap<String, Long> = mutableMapOf()
+
+fun scoreStrand(d: String, first: Int, last: Int, genes: Array<String>, health: Array<Int>): Long {
+    var score = 0L
     for (i in first..last) {
-        score += d.kmpMatchScore(genes[i])*health[i]
+        score += d.kmpMatchScore(genes[i]) * health[i]
     }
     return score
 }
 
-fun String.kmpMatchScore(pattern: String): Int {
-    var count = 0
+fun String.kmpMatchScore(pattern: String): Long {
+    return counts.getOrPut(pattern) {
+    var count = 0L
     val m = pattern.length
     val n = this.length
     val lps = pattern.computeLPS()
@@ -58,35 +77,37 @@ fun String.kmpMatchScore(pattern: String): Int {
         if (j == m) {
             count++
             j = lps[j - 1]
-        } else if (i<n && pattern[j]!=this[i]){
-            if (j!=0)
-                j=lps[j-1]
+        } else if (i < n && pattern[j] != this[i]) {
+            if (j != 0)
+                j = lps[j - 1]
             else
                 i++
         }
-
     }
     return count
+    }
 }
 
 fun String.computeLPS(): Array<Int> {
-    val m = this.length
-    val lps = Array<Int>(m) { 0 }
-    var i = 1
-    var len = 0
-    while (i < m) {
-        if (this[i] == this[len]) {
-            len++
-            lps[i] = len
-            i++
-        } else {
-            if (len != 0) {
-                len = lps[len - 1]
-            } else {
+    return lpss.getOrPut(this) {
+        val m = this.length
+        val lps = Array<Int>(m) { 0 }
+        var i = 1
+        var len = 0
+        while (i < m) {
+            if (this[i] == this[len]) {
+                len++
                 lps[i] = len
                 i++
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1]
+                } else {
+                    lps[i] = len
+                    i++
+                }
             }
         }
+        return lps
     }
-    return lps
 }
