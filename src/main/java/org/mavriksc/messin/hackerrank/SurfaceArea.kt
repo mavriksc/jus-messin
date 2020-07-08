ba251f68542e2bd65969fecabdfbe05fac869d6f
Sean Carlisle <scarlisle@hylamobile.com>
2020-07-08 12:44:50 -0500

    asdf

---
+++ src/main/java/org/mavriksc/messin/hackerrank/SurfaceArea.kt
@@ -0,0 +1,11 @@
+package org.mavriksc.messin.hackerrank
+
+//https://www.hackerrank.com/challenges/3d-surface-area/problem
+fun main() {
+    print(surfaceArea(emptyArray()))
+}
+
+fun surfaceArea(A: Array<Array<Int>>): Int {
+
+return 0
+}
