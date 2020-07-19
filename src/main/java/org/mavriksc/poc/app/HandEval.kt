7a0336127ab3ec03eebc608a9dce956bc56f0d99
Sean Carlisle <scarlisle@hylamobile.com>
2020-07-18 23:41:38 -0500

    all evals

--- src/main/java/org/mavriksc/poc/app/HandEval.kt
+++ src/main/java/org/mavriksc/poc/app/HandEval.kt
@@ -40,11 +40,17 @@ fun eval(cards: List<Card>): Int {
     hand = check1Pair(cards)
     if (hand.isNotEmpty())
         return rank1Pair(hand)
-    return highcard(cards)
+    hand = getHighCardHand(cards)
+    return highcard(hand)
 }
 
 fun highcard(hand: List<Card>): Int {
+
     return 0
+}
+
+fun getHighCardHand(cards: List<Card>): List<Card> {
+    return cards.sortedByDescending { it.rank() }.take(5)
 }
 
 fun rank1Pair(hand: List<Card>): Int {
@@ -52,7 +58,15 @@ fun rank1Pair(hand: List<Card>): Int {
 }
 
 fun check1Pair(cards: List<Card>): List<Card> {
-    return emptyList()
+    val rankMap = cards.groupBy { it.rank() }
+    val keys = rankMap.keys.sortedWith(compareBy({ rankMap[it]!!.size }, { it })).reversed()
+    return if (rankMap[keys[0]]!!.size == 2)
+        rankMap[keys[0]]!!
+                .union(cards.filterNot { it.rank() == keys[0] || it.rank() == keys[1] }
+                        .sortedByDescending { it.rank() }
+                        .take(3))
+                .toList()
+    else emptyList()
 }
 
 fun rank2Pair(hand: List<Card>): Int {
@@ -60,7 +74,17 @@ fun rank2Pair(hand: List<Card>): Int {
 }
 
 fun check2Pair(cards: List<Card>): List<Card> {
-    return emptyList()
+    val rankMap = cards.groupBy { it.rank() }
+    val keys = rankMap.keys.sortedWith(compareBy({ rankMap[it]!!.size }, { it })).reversed()
+    return if (rankMap[keys[0]]!!.size == 2 && rankMap[keys[1]]?.size!! == 2)
+        rankMap[keys[0]]!!
+                .union(rankMap[keys[1]]!!.take(2))
+                .union(cards.filterNot { it.rank() == keys[0] || it.rank() == keys[1] }
+                        .sortedByDescending { it.rank() }
+                        .take(1))
+                .toList()
+    else emptyList()
+
 }
 
 fun rank3OfAKind(hand: List<Card>): Int {
@@ -68,7 +92,13 @@ fun rank3OfAKind(hand: List<Card>): Int {
 }
 
 fun check3OfAKind(cards: List<Card>): List<Card> {
-    return emptyList()
+    val rankMap = cards.groupBy { it.rank() }
+    val trips = rankMap.filter { it.value.size == 3 }
+    return if (trips.isNotEmpty())
+        rankMap[trips.keys.first()]
+                ?.union(cards.filterNot { it.rank() == trips.keys.first() }
+                        .sortedByDescending { it.rank() }.take(2))?.toList()!!
+    else emptyList()
 }
 
 fun rankStraight(hand: List<Card>): Int {
@@ -81,7 +111,6 @@ fun checkStraight(cards: List<Card>): List<Card> {
         val itHighStraight = (it downTo it - 4).map { r -> r.toRank() }
         if (handRanks.containsAll(itHighStraight)) {
             val rank = Card.Rank.values()[it]
-            println("$rank high straight")
             return itHighStraight.map { cards.find { c -> c.rank() == it }!! }
         }
     }
@@ -93,11 +122,11 @@ fun rankFlush(hand: List<Card>): Int {
 }
 
 fun checkFlush(cards: List<Card>): List<Card> {
-    val isFlush  = cards.groupBy { it.suit() }.filter { it.value.size>=5 }
+    val isFlush = cards.groupBy { it.suit() }.filter { it.value.size >= 5 }
     return if (isFlush.isNotEmpty()) {
-         isFlush[isFlush.keys.first()]?.sortedByDescending { it.rank() }?.filterIndexed { index, _ -> index < 5 }!!
+        isFlush[isFlush.keys.first()]?.sortedByDescending { it.rank() }?.filterIndexed { index, _ -> index < 5 }!!
     } else
-     emptyList()
+        emptyList()
 }
 
 fun rankFullHouse(hand: List<Card>): Int {
@@ -105,7 +134,11 @@ fun rankFullHouse(hand: List<Card>): Int {
 }
 
 fun checkFullHouse(cards: List<Card>): List<Card> {
-    return emptyList()
+    val rankMap = cards.groupBy { it.rank() }
+    val keys = rankMap.keys.sortedWith(compareBy({ rankMap[it]!!.size }, { it })).reversed()
+    return if (rankMap[keys[0]]!!.size == 3 && rankMap[keys[1]]?.size!! > 2)
+        rankMap[keys[0]]!!.union(rankMap[keys[1]]!!.take(2)).toList()
+    else emptyList()
 }
 
 fun rank4OfAKind(hand: List<Card>): Int {
@@ -117,12 +150,21 @@ fun rankStraightFlush(hand: List<Card>): Int {
 }
 
 fun check4OfAKind(cards: List<Card>): List<Card> {
+    val rankMap = cards.groupBy { it.rank() }
+    val quads = rankMap.filter { it.value.size == 4 }
+    if (quads.isNotEmpty()) {
+        val kicker = rankMap[rankMap.keys.sorted().filterNot { it == quads.keys.first() }.last()]?.first()!!
+        return quads[quads.keys.first()]!!.union(listOf(kicker)).toList()
+    }
     return emptyList()
 }
 
 fun checkStraightFlush(cards: List<Card>): List<Card> {
-
-    return emptyList()
+    // TODO doesn't work for misses need to update
+    val flushCards = cards.groupBy { it.suit() }.filter { it.value.size >= 5 }
+    return if (flushCards.isNotEmpty())
+        flushCards[flushCards.keys.first()]?.let { checkStraight(it) }!!
+    else emptyList()
 }
 
 fun Int.toRank(): Card.Rank {
--- src/main/java/org/mavriksc/poc/app/TestCardStuff.kt
+++ src/main/java/org/mavriksc/poc/app/TestCardStuff.kt
@@ -3,16 +3,16 @@ package org.mavriksc.poc.app
 import org.mavriksc.poc.model.Card
 
 fun main() {
-    val hand = listOf(Card(Card.Rank.EIGHT, Card.Suit.DIAMONDS),
-            Card(Card.Rank.FOUR, Card.Suit.DIAMONDS),
-            Card(Card.Rank.THREE, Card.Suit.CLUBS),
-            Card(Card.Rank.TWO, Card.Suit.DIAMONDS),
+    val hand = listOf(Card(Card.Rank.SIX, Card.Suit.DIAMONDS),
+            Card(Card.Rank.SIX, Card.Suit.CLUBS),
+            Card(Card.Rank.SIX, Card.Suit.HEARTS),
+            Card(Card.Rank.EIGHT, Card.Suit.DIAMONDS),
             Card(Card.Rank.ACE, Card.Suit.DIAMONDS),
-            Card(Card.Rank.FIVE, Card.Suit.DIAMONDS),
-            Card(Card.Rank.JACK, Card.Suit.DIAMONDS))
+            Card(Card.Rank.EIGHT, Card.Suit.HEARTS),
+            Card(Card.Rank.EIGHT, Card.Suit.CLUBS))
 
-    val straight = checkFlush(hand)
-    straight.map { print("$it ") }
+    val fours = check3OfAKind(hand)
+    fours.map { print("$it ") }
 
 
 }
