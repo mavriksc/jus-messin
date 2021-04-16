ff6bc9c2908ecfe10d886da67a14bd5f87428cb5
Sean Carlisle <scarlisle@hylamobile.com>
2021-04-15 23:35:34 -0500

    performance fix

--- src/main/java/org/mavriksc/messin/CustomSort.java
+++ src/main/java/org/mavriksc/messin/CustomSort.java
@@ -1,46 +1,77 @@
 package org.mavriksc.messin;
 
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class CustomSort {
 
     public static void main(String[] args) {
-        String[] arr = {"2T1BURHE1JCO24154C",
-                "2TABURHE1JC024154C",
-                "JTDKARFP5H3055472C",
-                "2T2BURHE1JCO24154C",
-                "JTDKARFP1H3056246C",
-                "JTDKARFP1H3056246"};
 
-        Comparator<String> c = (s1, s2) -> {
+        List<String> l1 = makeRandomList();
+        List<String> l2 = new ArrayList<>(l1);
+
+        String order = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+
+        Comparator<String> comparatorNew = (s1, s2) -> {
             int len1 = s1.length();
             int len2 = s2.length();
             int lim = Math.min(len1, len2);
-            char[] v1 = s1.toCharArray();
-            char[] v2 = s2.toCharArray();
-
             int k = 0;
             while (k < lim) {
-                char c1 = v1[k];
-                char c2 = v2[k];
+                char c1 = s1.charAt(k);
+                char c2 = s2.charAt(k);
                 if (c1 != c2) {
                     if ((c1 >= 'A' && c2 >= 'A') ^ (c1 <= '9' && c2 <= '9')) {
                         return c1 - c2;
                     } else {
-                        System.out.println(c2 - c1);
                         return c2 - c1;
-                   
                     }
                 }
                 k++;
             }
             return len1 - len2;
         };
-        Arrays.sort(arr, c);
-        for (
-                String s : arr) {
-            System.out.println(s);
-        }
+        Comparator<String> comparatorOld = (s1, s2) -> {
+            int minLen = Math.min(s1.length(), s2.length());
+            for (int i = 0; i < minLen; i++) {
+                char s1Char = s1.charAt(i);
+                char s2Char = s2.charAt(i);
+                if (s1Char != s2Char) {
+                    //if the characters are diff get precedence
+                    return order.indexOf(s1Char) - order.indexOf(s2Char);
+                }
+            }
+            // if all chars are the same the longer one is last
+            return s1.length() - s2.length();
+        };
+
+        Date start, end = null;
+        long t1, t2 = 0;
+        start = new Date();
+        l1.sort(comparatorNew);
+        end = new Date();
+        t1 = end.getTime() - start.getTime();
+        start = new Date();
+        l2.sort(comparatorOld);
+        end = new Date();
+        t2 = end.getTime() - start.getTime();
+        System.out.println("NEW METHOD:" + t1);
+        System.out.println("OLD METHOD:" + t2);
     }
+
+    private static List<String> makeRandomList() {
+        int min = 15;
+        int max = 20;
+        return IntStream.range(0, 1_000_000).mapToObj(i ->
+                IntStream.range(0, ThreadLocalRandom.current().nextInt(min, max))
+                        .map(x -> ThreadLocalRandom.current().nextInt(36))
+                        .map(x -> x < 10 ? (char) ('0' + x) : (char) ('A' + x - 10))
+                        .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString()
+        ).collect(Collectors.toList());
+    }
+
+
 }
