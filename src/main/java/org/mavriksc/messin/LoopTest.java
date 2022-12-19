45dd0c42f36d9b2891106f4408af38da49cead4e
Sean Carlisle <s.m.carlisle@gmail.com>
2022-12-18 21:17:18 -0600

    workflows

---
+++ src/main/java/org/mavriksc/messin/LoopTest.java
@@ -0,0 +1,21 @@
+package org.mavriksc.messin;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class LoopTest {
+    private static List<Integer> list;
+    private static final int size=1000;
+
+    public static void main(String[] args) {
+
+    }
+    private static void genList(){
+        list = new ArrayList<Integer>(size);
+        for (int i = 0; i < size; i++) {
+            list.set(i, ThreadLocalRandom.current().nextInt(0,200));
+        }
+    }
+}
