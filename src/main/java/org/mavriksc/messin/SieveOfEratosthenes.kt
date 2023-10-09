49ca678c4f4c01ffa7375d790b2458ba46d52ff2
Sean Carlisle <s.m.carlisle@gmail.com>
2023-10-08 20:56:33 -0500

    1 off error at end of chunk was causing some misses. there was also a double increment that was fixed from the first imp

--- src/main/java/org/mavriksc/messin/SieveOfEratosthenes.kt
+++ src/main/java/org/mavriksc/messin/SieveOfEratosthenes.kt
@@ -3,9 +3,10 @@ package org.mavriksc.messin
 import java.io.File
 import java.math.BigInteger
 
+// This works. there was 1 off in the outer while of the segment 
 
 val writeToPrimeFile = File("primes2.txt")
-val chunkSize = BigInteger("20000")
+val chunkSize = BigInteger("200000")
 val N = BigInteger("10000000")
 
 fun main() {
@@ -17,39 +18,14 @@ fun main() {
                 ++chunkNum * chunkSize - BigInteger.ONE
             ).joinToString("\n")
         )
-        chunkNum++
     }
-
 }
-
-//https://www.geeksforgeeks.org/sieve-of-eratosthenes/
-fun sieveOfEratosthenes(n: Int): List<Int> {
-    // Create a boolean array
-    // "prime[0..n]" and
-    // initialize all entries
-    // it as true. A value in
-    // prime[i] will finally be
-    // false if i is Not a
-    // prime, else true.
-    val sieve = BooleanArray(n + 1) { true }
-    val primes = mutableListOf<Int>()
-    for (p in 2..n) {
-        if (sieve[p]) {
-            primes.add(p)
-            for (i in p * p..n step p) {
-                sieve[i] = false
-            }
-        }
-    }
-    return primes
-}
-
 
 fun segmentedSieve(firstValue: BigInteger, lastValue: BigInteger): List<BigInteger> {
     val sieve = BooleanArray((lastValue - firstValue).toInt() + 1) { true }
     val primes = mutableListOf<BigInteger>()
     var current = BigInteger("2")
-    while (current < lastValue) {
+    while (current <= lastValue) {
         // number is in the interval it may be prime check and then do the sweep
         if (firstValue < current && sieve[(current - firstValue).toInt()]) {
             primes.add(current)
