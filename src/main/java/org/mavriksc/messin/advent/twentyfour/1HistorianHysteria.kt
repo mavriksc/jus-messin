e73e96b7c829c8df9dff67a65f1e5af415634ac6
Sean Carlisle <s.m.carlisle@gmail.com>
2024-12-01 11:39:19 -0600

    map is going out of bounds for each works and is faster

--- src/main/java/org/mavriksc/messin/advent/twentyfour/1HistorianHysteria.kt
+++ src/main/java/org/mavriksc/messin/advent/twentyfour/1HistorianHysteria.kt
@@ -1,31 +1,46 @@
 package org.mavriksc.messin.advent.twentyfour
 
-import org.mavriksc.messin.mapLines
 import org.mavriksc.messin.readFile
+import org.mavriksc.messin.toReader
 import kotlin.math.abs
+
 // create a read file function that allows you to pass in an operation maybe
 
 
 fun main() {
-    //partOne()
+    var start = System.currentTimeMillis()
+    partOne()
+    println("${System.currentTimeMillis() - start} ms")
     //partTwo()
+    start = System.currentTimeMillis()
     tryUseLines()
+    println("${System.currentTimeMillis() - start} ms")
 
 }
 
 fun tryUseLines() {
-    "advent/24/one/test.txt".mapLines { it.split(" ") }
+    val left = mutableListOf<Int>()
+    val right = mutableListOf<Int>()
+    "advent/24/one/input.txt".toReader().forEachLine {
+        val parts = it.split(" ")
+        left.add(parts[0].toInt())
+        right.add(parts[3].toInt())
+    }
+    right.sort()
+    val answer = left.sorted().mapIndexed { index: Int, i: Int -> abs(right[index] - i) }.sum()
+    println(answer)
+
 }
 
 fun partTwo() {
     val lines = "advent/24/one/input.txt".readFile()
-    val lists = twoSortedLists(lines,false)
+    val lists = twoSortedLists(lines, false)
     val counts = lists.second.groupingBy { it }.eachCount()
     val answer = lists.first.map { counts.getOrDefault(it, 0) * it }.sum()
     println(answer)
 }
 
-fun partOne(){
+fun partOne() {
     val lines = "advent/24/one/input.txt".readFile()
     val lists = twoSortedLists(lines)
     val answer = lists.first.mapIndexed { index: Int, i: Int -> abs(lists.second[index] - i) }.sum()
