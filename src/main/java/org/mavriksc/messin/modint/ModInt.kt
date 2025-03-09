c69206c62bf10776aadbd54f03558f137aa926c0
Sean Carlisle <s.m.carlisle@gmail.com>
2025-03-09 11:09:41 -0500

    things

---
+++ src/main/java/org/mavriksc/messin/modint/ModInt.kt
@@ -0,0 +1,33 @@
+package org.mavriksc.messin.modint
+
+import kotlin.properties.Delegates
+
+var modulo = 69
+var i: Int by Delegates.observable(0) { property, oldValue, newValue ->
+    if (newValue == oldValue)
+        println("didnt change")
+    else {
+        println("new value $newValue")
+        i = newValue % modulo
+        println("set value $i")
+    }
+}
+
+
+fun main() {
+    i = 67
+    i++
+    println(i)
+    i++
+    println(i)
+    i++
+    println(i)
+    i++
+    println(i)
+    i++
+    println(i)
+    i++
+    println(i)
+    i++
+    println(i)
+}
