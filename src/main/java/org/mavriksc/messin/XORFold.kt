62e672290eae36c29566d70b16c4d5895d244a59
Sean Carlisle <s.m.carlisle@gmail.com>
2025-07-14 19:04:07 -0500

    xor stuff

--- src/main/java/org/mavriksc/messin/XORFold.kt
+++ src/main/java/org/mavriksc/messin/XORFold.kt
@@ -14,12 +14,12 @@ fun findOneMissing(){
 fun findTwoMissing(){
     val o = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
     val l = o.shuffled().dropLast(2)
-    val uXv = o.reduce { a, b -> a xor b } xor l.reduce { a, b -> a xor b }
-    val lsb = uXv and -uXv
-    val pot = o.filter { it and lsb == 1 }.reduce { a, b -> a xor b }
-    val pom = l.filter { it and lsb == 1 }.reduce { a, b -> a xor b }
-    val v = pot xor pom
-    val u = uXv xor v
+    val uXv = o.reduce { a, b -> a xor b } xor l.reduce { a, b -> a xor b } // the 2 values compressed as u xor v
+    val lsb = uXv and -uXv // find a place where u and v are different lsb is fine
+    val pot = o.filter { it and lsb == 1 }.reduce { a, b -> a xor b }// get partition of the original list where lsb is 1 and reduce
+    val pom = l.filter { it and lsb == 1 }.reduce { a, b -> a xor b }// get partition of the missing list where lsb is 1 and reduce
+    val v = pot xor pom // it's now 1 value missing in 1 list
+    val u = uXv xor v // get u now that we know v
     println(l)
     println("$u, $v")
 
